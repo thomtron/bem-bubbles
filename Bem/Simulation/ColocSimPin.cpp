eed3e5a0038ecbd7fd2150dc80d920864a76fa96
threnggli <threnggli@student.ethz.ch>
2023-10-15 21:42:47 +0200

    corrected dumb error (1-t) instead of t

--- Bem/Simulation/ColocSimPin.cpp
+++ Bem/Simulation/ColocSimPin.cpp
@@ -512,10 +512,8 @@ void ColocSimPin::remesh(real L) {
         borderprojects.trigs.push_back(Triplet(trigind_offset,trigind_offset+2,trigind_offset+3));
         trigind_offset += 4;
 
-        //t_min = 1.0 - t_min;
-
-        real phi_int = t_min*phi(perm[ind_min]) + (1.0-t_min)*phi(perm[(ind_min+1)%kp]);
-        real psi_int = t_min*psi(perm[ind_min]) + (1.0-t_min)*psi(perm[(ind_min+1)%kp]);
+        real phi_int = (1.0-t_min)*phi(perm[ind_min]) + t_min*phi(perm[(ind_min+1)%kp]);
+        real psi_int = (1.0-t_min)*psi(perm[ind_min]) + t_min*psi(perm[(ind_min+1)%kp]);
 
         borderpots.push_back(0.0);
         borderpots.push_back(phi(perm[ind_min]));
