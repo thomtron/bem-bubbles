75547628b0278f231c21c6af50662aec6bbf067f
Thomas Renggli <thomas.renggli@bluewin.ch>
2023-10-30 18:46:51 +0100

    changed beta

--- pinned-bubble.cpp
+++ pinned-bubble.cpp
@@ -37,7 +37,7 @@ int main() {
 
     Bem::real radius   = 128e-6; // m
     Bem::real pressure = 15e3; // Pa
-    string folder = "pinned-beta/f=30e3_r=128e-6_p=15e3_beta=1.0_rem=0.15_epsilon=1e-2/";
+    string folder = "pinned-beta/f=30e3_r=128e-6_p=15e3_beta=0.5_rem=0.15_epsilon=1e-2/";
     
     cout << "radius:   " << radius << endl;
     cout << "pressure: " << pressure << endl;
