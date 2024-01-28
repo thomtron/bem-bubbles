3c8652794310b648a47459709bc499c5d9f6abe2
Thomas Renggli <thomas.renggli@bluewin.ch>
2024-01-29 00:06:46 +0100

    new sim

--- pinned-bubble.cpp
+++ pinned-bubble.cpp
@@ -58,9 +58,9 @@ int main() {
 
     // following lines borrowed from oscillations.cpp
 
-    Bem::real radius   = 85e-6; // m 
-    Bem::real pressure = 1e3; // Pa
-    string folder = "pinned-beta/f=30e3_r=85e-6_p=1e3_beta=0.2_rem=0.12_epsilon=1e-2_b-nonlin-0.01-env/";
+    Bem::real radius   = 130e-6; // m 
+    Bem::real pressure = 12e3; // Pa
+    string folder = "pinned-beta/f=30e3_r=130e-6_p=12e3_beta=0.2_rem=0.12_epsilon=1e-2_b-nonlin-0.01-env/";
     
     cout << "radius:   " << radius << endl;
     cout << "pressure: " << pressure << endl;
