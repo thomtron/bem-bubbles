63df6dfc080afcd3f93c8ac1d535abdaef3e1511
Thomas Renggli <thomas.renggli@bluewin.ch>
2024-10-03 17:39:55 +0200

    change prams

--- pinned-bubble.cpp
+++ pinned-bubble.cpp
@@ -65,9 +65,9 @@ int main() {
 
     // following lines borrowed from oscillations.cpp
 
-    Bem::real radius   = 90e-6; // m 
-    Bem::real pressure = 1.8e3; // Pa
-    string folder = "/cluster/home/threnggli/results/f=30e3_r=90e-6_p=1.8e3_beta=0.2_rem=0.12_epsilon=1e-2_b-nonlin-0.01-smo/";
+    Bem::real radius   = 115e-6; // m 
+    Bem::real pressure = 15e3; // Pa
+    string folder = "/cluster/home/threnggli/results/f=30e3_r=115e-6_p=15e3_beta=0.2_rem=0.12_epsilon=1e-2_b-nonlin-0.01-smo/";
     
     cout << "radius:   " << radius << endl;
     cout << "pressure: " << pressure << endl;
