ab967144b7a30cb1d9cc458c1bca7e4a0873a181
Thomas Renggli <thomas.renggli@bluewin.ch>
2024-10-26 00:09:03 +0200

    new params

--- pinned-bubble.cpp
+++ pinned-bubble.cpp
@@ -66,8 +66,8 @@ int main() {
     // following lines borrowed from oscillations.cpp
 
     Bem::real radius   = 70e-6; // m 
-    Bem::real pressure = 10e3; // Pa
-    string folder = "/cluster/home/threnggli/results/f=30e3_r=70e-6_p=10e3_beta=0.2_rem=0.1_epsilon=1e-2_b-nonlin-0.01-smo-fine/";
+    Bem::real pressure = 15e3; // Pa
+    string folder = "/cluster/home/threnggli/results/f=30e3_r=70e-6_p=15e3_beta=0.2_rem=0.1_epsilon=1e-2_b-nonlin-0.01-smo-fine/";
     
     cout << "radius:   " << radius << endl;
     cout << "pressure: " << pressure << endl;
