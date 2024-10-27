272798f5e3e34b789d580cea137f52f423a86eee
Thomas Renggli <thomas.renggli@bluewin.ch>
2024-10-28 00:20:30 +0100

    slow sim

--- pinned-bubble.cpp
+++ pinned-bubble.cpp
@@ -65,9 +65,9 @@ int main() {
 
     // following lines borrowed from oscillations.cpp
 
-    Bem::real radius   = 135e-6; // m 
-    Bem::real pressure = 20e3; // Pa
-    string folder = "/cluster/home/threnggli/results/f=30e3_r=135e-6_p=20e3_beta=0.2_rem=0.1_epsilon=1e-2_b-nonlin-0.01-smo-fine/";
+    Bem::real radius   = 70e-6; // m 
+    Bem::real pressure = 15e3; // Pa
+    string folder = "/cluster/home/threnggli/results/f=30e3_r=70e-6_p=15e3_beta=0.2_rem=0.1_epsilon=1e-2_b-nonlin-0.01-smo-fine-slow/";
     
     cout << "radius:   " << radius << endl;
     cout << "pressure: " << pressure << endl;
@@ -115,7 +115,7 @@ int main() {
 
     
     
-    Bem::real dp = 0.005;
+    Bem::real dp = 0.002;
     size_t N(10000);
     
     ColocSimPin sim(M,P_ref,P_gas0,Sigma,Gamma,&waveform);
